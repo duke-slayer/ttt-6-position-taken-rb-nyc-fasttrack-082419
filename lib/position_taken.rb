32cfe8a20f3304f08d2f2d76a6a5d3b5f36cec25
Amy Duke <amyyyduke@gmail.com>
2019-08-24 19:51:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == ""  || board[index] == ""
     return false
-  else board == "X" || board == "O"
+  else board[index] == "X" || board[index] == "O"
     return true
   end
 end 
